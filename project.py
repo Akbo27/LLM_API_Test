3ca90dd67145862277a1115097a6cf2617145eb8
Akbo27 <aungkaungbo.edu@gmail.com>
2025-01-25 17:35:29 +0700

    API Implementation done

--- project.py
+++ project.py
@@ -3,3 +3,19 @@ import os
 import dotenv
 from mistralai import Mistral
 
+dotenv.load_dotenv()
+
+api_key = os.environ["MISTRAL_API_KEY"]
+model = "mistral-larget-latest"
+
+client = Mistral(api_key=api_key)
+
+chat_response = client.chat.complete(
+    model = model
+    messages= [
+        {
+            "role":"user",
+            "content": "Hi! I need a plot of sine function on interval from -5 to 5"
+        }
+    ]
+)
